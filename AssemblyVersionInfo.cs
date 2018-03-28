954a2a9d6d939ab7178b4a7c296f5a0fb7899b5e
minhnguyenlinknode <quy.minh@yahoo.com>
2018-03-28 12:09:41 +0100

    Added version info

---
+++ AssemblyVersionInfo.cs
@@ -0,0 +1,21 @@
+﻿	// <copyright file="AssemblyVersionInfo.cs" company="Firefish Software Ltd">
+//     Copyright (c) Firefish Software Ltd. All rights reserved.
+// </copyright>
+
+// Version information for an assembly consists of the following four values:
+//      Traditional values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      SVN Revision
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+//      Our values
+//      Firefish's age (increments every Jan 8th)
+//      Release version (resets when age increments)
+//      Hotfix Number (resets every release)
+//      SVN Revision
+//
+[assembly: System.Reflection.AssemblyVersion("3.1.21.30985")]
+[assembly: System.Reflection.AssemblyFileVersion("3.1.21.30985")]
